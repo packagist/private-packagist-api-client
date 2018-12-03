912bed1385df47f0b4d3b8da20f1126c101598f3
Stephan Vock <stephan.vock@gmail.com>
2018-12-03 16:58:19 +0100

    Project: add token endpoints

--- src/Api/Projects.php
+++ src/Api/Projects.php
@@ -11,9 +11,9 @@ class Projects extends AbstractApi
         return $this->get('/projects/');
     }
 
-    public function show($projectId)
+    public function show($projectName)
     {
-        return $this->get(sprintf('/projects/%s/', $projectId));
+        return $this->get(sprintf('/projects/%s/', $projectName));
     }
 
     public function create($name)
@@ -21,17 +21,17 @@ class Projects extends AbstractApi
         return $this->post('/projects/', ['name' => $name]);
     }
 
-    public function remove($projectId)
+    public function remove($projectName)
     {
-        return $this->delete(sprintf('/projects/%s/', $projectId));
+        return $this->delete(sprintf('/projects/%s/', $projectName));
     }
 
-    public function listTeams($projectsId)
+    public function listTeams($projectsName)
     {
-        return $this->get(sprintf('/projects/%s/teams/', $projectsId));
+        return $this->get(sprintf('/projects/%s/teams/', $projectsName));
     }
 
-    public function addOrUpdateTeams($projectId, array $teams)
+    public function addOrUpdateTeams($projectName, array $teams)
     {
         foreach ($teams as $team) {
             if (!isset($team['id'])) {
@@ -43,16 +43,40 @@ class Projects extends AbstractApi
             }
         }
 
-        return $this->post(sprintf('/projects/%s/teams/', $projectId), $teams);
+        return $this->post(sprintf('/projects/%s/teams/', $projectName), $teams);
     }
 
-    public function removeTeam($projectId, $teamId)
+    public function removeTeam($projectName, $teamId)
     {
-        return $this->delete(sprintf('/projects/%s/teams/%s/', $projectId, $teamId));
+        return $this->delete(sprintf('/projects/%s/teams/%s/', $projectName, $teamId));
     }
 
-    public function listPackages($projectsId)
+    public function listPackages($projectName)
     {
-        return $this->get(sprintf('/projects/%s/packages/', $projectsId));
+        return $this->get(sprintf('/projects/%s/packages/', $projectName));
+    }
+
+    public function listTokens($projectName)
+    {
+        return $this->get(sprintf('/projects/%s/tokens/', $projectName));
+    }
+
+    public function createToken($projectName, array $tokenData)
+    {
+        return $this->post(sprintf('/projects/%s/tokens/', $projectName), $tokenData);
+    }
+
+    public function removeToken($projectName, $tokenId)
+    {
+        return $this->delete(sprintf('/projects/%s/tokens/%s/', $projectName, $tokenId));
+    }
+
+    public function regenerateToken($projectName, $tokenId, array $confirmation)
+    {
+        if (!isset($confirmation['IConfirmOldTokenWillStopWorkingImmediately'])) {
+            throw new InvalidArgumentException('Confirmation is required to regenerate the Composer repository token.');
+        }
+
+        return $this->post(sprintf('/projects/%s/tokens/%s/regenerate', $projectName, $tokenId), $confirmation);
     }
 }
