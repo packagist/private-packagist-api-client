9f8d3411a1951aa26447319c7605829e79832a5b
Stephan Vock <stephan.vock@gmail.com>
2019-02-06 13:53:32 +0100

    Files: add license header to all files

--- tests/HttpClient/Plugin/RequestSignatureTest.php
+++ tests/HttpClient/Plugin/RequestSignatureTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * (c) Packagist Conductors UG (haftungsbeschränkt) <contact@packagist.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PrivatePackagist\ApiClient\HttpClient\Plugin;
 
 use GuzzleHttp\Psr7\Request;
