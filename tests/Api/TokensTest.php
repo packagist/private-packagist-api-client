88cd58653958b674c15d5fb50812e84e1df12eca
Stephan Vock <stephan.vock@gmail.com>
2022-01-13 15:40:03 +0000

    Tests: add missing file header

--- tests/Api/TokensTest.php
+++ tests/Api/TokensTest.php
@@ -1,4 +1,11 @@
-<?php declare(strict_types=1);
+<?php
+
+/*
+ * (c) Packagist Conductors GmbH <contact@packagist.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace PrivatePackagist\ApiClient\Api;
 
