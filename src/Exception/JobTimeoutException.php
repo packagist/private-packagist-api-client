34812d96c4574975e69b2e8f614b37c323d7ff3f
Stephan Vock <stephan.vock@gmail.com>
2024-03-26 15:51:25 +0000

    PHP-CS-Fixer: automatically add file header

--- src/Exception/JobTimeoutException.php
+++ src/Exception/JobTimeoutException.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * (c) Packagist Conductors GmbH <contact@packagist.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PrivatePackagist\ApiClient\Exception;
 
 class JobTimeoutException extends RuntimeException
--- src/Payload/CustomPackageConfig.php
+++ src/Payload/CustomPackageConfig.php
@@ -1,5 +1,12 @@
 <?php declare(strict_types=1);
 
+/**
+ * (c) Packagist Conductors GmbH <contact@packagist.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PrivatePackagist\ApiClient\Payload;
 
 /**
